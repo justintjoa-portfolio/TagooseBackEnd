1dbc456d552e6fe78cee106e96891d1a2a54061d
justintjoa <40640455+justintjoa@users.noreply.github.com>
2020-04-26 11:19:14 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask import request
+from keys import username
+from keys import accountpassword
 import mysql.connector
 import json
 
@@ -8,8 +10,8 @@ process = Blueprint('process', __name__)
 @process.route("/", methods=['POST','GET'])
 def processfunc():
     data = request.get_json()
-    connection = mysql.connector.connect(user='root',
-                              password='fOrtranmyeggo124',
+    connection = mysql.connector.connect(user=username,
+                              password=accountpassword,
                               host='tagoosedev.cm63orfguism.us-east-1.rds.amazonaws.com',
                               database='USER_DATA')
                 
